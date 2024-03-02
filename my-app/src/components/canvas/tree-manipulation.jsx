8ac0db78c311c9d359d9a9ea1340ba71281715f5
Ryunosuke39 <111721351+Ryunosuke39@users.noreply.github.com>
2024-03-03 00:54:39 +1100

    line between each node coded

--- my-app/src/components/canvas/tree-manipulation.jsx
+++ my-app/src/components/canvas/tree-manipulation.jsx
@@ -14,30 +14,151 @@ export default function RenderTree() {
     this.x = x
     this.y = y
 
+    // circle
     ctx.beginPath()
-    ctx.arc(this.x, this.y, 30, 0, Math.PI * 2)
+    ctx.arc(this.x, this.y, 20, 0, Math.PI * 2)
     ctx.strokeStyle = 'blue'
     ctx.stroke()
 
-    ctx.font = '30px Arial'
+    // line
+    // ctx.moveTo(this.x, this.y)
+    // ctx.lineTo(100, 100)
+    // ctx.strokeStyle = 'blue'
+    // ctx.stroke()
+
+    // text
+    ctx.font = '20px Arial'
     ctx.fillText(this.num, this.x, this.y)
+  }
+
+  // get canvas node based on the weight its passed
+  function getCanvasNodeFromNode(num, nodesArr) {
+    if (nodesArr && nodesArr.length) {
+      for (let i = 0; i < nodesArr.length; i++) {
+        if (num === nodesArr[i].num) {
+          return nodesArr[i]
+        }
+      }
+      return false
+    }
   }
 
   // the root should not be the canvas node
   // need to get access to left, right
-  function RecPlaceCanvasNode(root, num, x, y, ctx) {
+  function RecPlaceCanvasNode(
+    root,
+    num,
+    x,
+    y,
+    ctx,
+    Lcount,
+    Rcount,
+    nodesArr,
+    LArr,
+    RArr
+  ) {
     if (root == null) {
       let root = new CanvasNode(num, x, y, ctx)
+      // avoid putting node on the existing node:
+      nodesArr.push(root)
+      // check current node postion with existing node pos
+      if (LArr && LArr.length) {
+        //same as RArr
+        for (let i = 0; i < LArr.length; i++) {
+          if (LArr[i] === Lcount && RArr[i] === Rcount) {
+            console.log(`node ${JSON.stringify(root)} is on the other node`)
+            // excet root
+            ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+            nodesArr[i + 1].x = x - 50
+            for (let j = 0; j < nodesArr.length; j++) {
+              root = new CanvasNode(
+                nodesArr[j].num,
+                nodesArr[j].x,
+                nodesArr[j].y,
+                ctx
+              )
+            }
+            // root = new CanvasNode(num, x, y, ctx)
+          }
+        }
+      }
+      // push L, R count as existing node
+      LArr.push(Lcount)
+      RArr.push(Rcount)
+
+      // line
+      for (let k = 0; k < shaffledArr.length; k++) {
+        // tempNode is node from pure data structure js
+        let tempNode = generatedTree.search(shaffledArr[k])
+        for (let l = 0; l < nodesArr.length; l++) {
+          if (tempNode.weight === nodesArr[l].num) {
+            let tempL = tempNode.left
+            let tempR = tempNode.right
+            if (tempL !== null) {
+              let tempCanvasNode = getCanvasNodeFromNode(tempL.weight, nodesArr)
+              if (tempCanvasNode) {
+                ctx.moveTo(nodesArr[l].x, nodesArr[l].y)
+                ctx.lineTo(tempCanvasNode.x, tempCanvasNode.y)
+                ctx.strokeStyle = 'blue'
+                ctx.stroke()
+              }
+            }
+            if (tempR !== null) {
+              let tempCanvasNode = getCanvasNodeFromNode(tempR.weight, nodesArr)
+              if (tempCanvasNode) {
+                ctx.moveTo(nodesArr[l].x, nodesArr[l].y)
+                ctx.lineTo(tempCanvasNode.x, tempCanvasNode.y)
+                ctx.strokeStyle = 'blue'
+                ctx.stroke()
+              }
+            }
+
+            // ctx.lineTo(100, 100)
+            // ctx.strokeStyle = 'blue'
+            // ctx.stroke()
+          }
+        }
+      }
+
       return root
     }
     if (num < root.num) {
-      x = x - 120
-      y = y + 100
-      root.left = RecPlaceCanvasNode(root.left, num, x, y, ctx)
+      Lcount++
+
+      // change pos
+      x = x - 90
+      y = y + 50
+
+      root.left = RecPlaceCanvasNode(
+        root.left,
+        num,
+        x,
+        y,
+        ctx,
+        Lcount,
+        Rcount,
+        nodesArr,
+        LArr,
+        RArr
+      )
     } else if (num > root.num) {
-      x = x + 120
-      y = y + 100
-      root.right = RecPlaceCanvasNode(root.right, num, x, y, ctx)
+      Rcount++
+      // change pos
+      x = x + 90
+      y = y + 50
+
+      root.right = RecPlaceCanvasNode(
+        root.right,
+        num,
+        x,
+        y,
+        ctx,
+        Lcount,
+        Rcount,
+        nodesArr,
+        LArr,
+        RArr
+      )
     }
     return root
   }
@@ -51,19 +172,37 @@ export default function RenderTree() {
 
     if (shaffledArr && shaffledArr.length) {
       let root = null
-      let compareNode = null
+      let canvasNodes = []
+      let leftCountArray = []
+      let rightCountArray = []
 
-      for (let i = 0; i < shaffledArr.length; i++) {
+      for (let i = 0; i < shaffledArr.length + 1; i++) {
         // set root node
         if (i === 0) {
           root = new CanvasNode(shaffledArr[i], c.canvas.width / 2, 100, c)
-          compareNode = root
+          canvasNodes.push(root)
         } else {
           // call recursive function
           // RecPlaceCanvasNode(root, num, x, y, ctx)
-          RecPlaceCanvasNode(root, shaffledArr[i], root.x, root.y, c)
+          let enterRightCount = 0
+          let enterLeftCount = 0
+          RecPlaceCanvasNode(
+            root,
+            shaffledArr[i],
+            root.x,
+            root.y,
+            c,
+            enterLeftCount,
+            enterRightCount,
+            canvasNodes,
+            leftCountArray,
+            rightCountArray
+          )
         }
       }
+      // test
+      console.log('Larr: ', leftCountArray)
+      console.log('Rare: ', rightCountArray)
     }
   }, [shaffledArr])
   return <canvas ref={canvasRef}></canvas>
